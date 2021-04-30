415a193f424cfefcf707d8c2100254a9f08bc4d3
Krysov <>
2021-04-30 03:27:11 +0200

    [~] set crossword model methods to be static; [=] version bump;

--- shared/src/commonMain/kotlin/kmm/example/app/shared/game/data/CrosswordModel.kt
+++ shared/src/commonMain/kotlin/kmm/example/app/shared/game/data/CrosswordModel.kt
@@ -9,44 +9,37 @@ data class CrosswordModel(
     val words: List<WordModel>,
 ) {
     val letters: Map<String, LetterModel> by lazy { getLetterMap(words) }
-
     fun getLetterAt(x: Short, y: Short): LetterModel? = letters[getLetterTableKey(x, y)]
+}
 
-    private fun getLetterMap(fromWords: List<WordModel>): Map<String, LetterModel> {
-        val result: MutableMap<String, LetterModel> = HashMap(fromWords.count())
-        fromWords
-            .sortedBy { it.index }
-            .forEach { unpackWord(it, result) }
-        return result
-    }
+private fun getLetterMap(fromWords: List<WordModel>): Map<String, LetterModel> {
+    val result: MutableMap<String, LetterModel> = HashMap(fromWords.count())
+    fromWords
+        .sortedBy { it.index }
+        .forEach { unpackWord(it, result) }
+    return result
+}
 
-    private fun unpackWord(word: WordModel, intoLetterTable: MutableMap<String, LetterModel>) {
-        word.answer.forEachIndexed { index, letter ->
-            val (x, y) = word.getLetterPosition(index)
-            val key = getLetterTableKey(x, y)
-            val existingLetter = intoLetterTable[key]
-            existingLetter?.let { existing ->
-                if (letter != existing.letter) throw IllegalArgumentException(
-                    """Letter '${letter}' does not match letter '${existing.letter}' at [${x};${y}] 
+private fun unpackWord(word: WordModel, intoLetterTable: MutableMap<String, LetterModel>) {
+    word.answer.forEachIndexed { index, letter ->
+        val (x, y) = word.getLetterPosition(index)
+        val key = getLetterTableKey(x, y)
+        val existingLetter = intoLetterTable[key]
+        existingLetter?.let { existing ->
+            if (letter != existing.letter) throw IllegalArgumentException(
+                """Letter '${letter}' does not match letter '${existing.letter}' at [${x};${y}] 
                         for the intersecting words: ${
-                        existing.words.plus(word).map { it.answer }
-                    }""".trimIndent()
-                )
-            }
-            val inductedLetter =
-                existingLetter?.copy(
-                    words = existingLetter.words.plus(word),
-                    indices = existingLetter.indices.plus(index.toShort()),
-                ) ?: LetterModel(letter, listOf(index.toShort()), listOf(word))
-            intoLetterTable[key] = inductedLetter
+                    existing.words.plus(word).map { it.answer }
+                }""".trimIndent()
+            )
         }
-    }
-
-    private fun getLetterTableKey(x: Short, y: Short): String = "${x}_${y}"
-
-    private fun getWordMap(forWordList: List<WordModel>): Map<String, WordModel> {
-        val result = HashMap<String, WordModel>(forWordList.count())
-        forWordList.forEach { word -> result[word.index] = word }
-        return result
+        val inductedLetter =
+            existingLetter?.copy(
+                words = existingLetter.words.plus(word),
+                indices = existingLetter.indices.plus(index.toShort()),
+            ) ?: LetterModel(letter, listOf(index.toShort()), listOf(word))
+        intoLetterTable[key] = inductedLetter
     }
 }
+
+private fun getLetterTableKey(x: Short, y: Short): String = "${x}_${y}"
